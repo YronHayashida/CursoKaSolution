77fc810bd1dec2358cbd022d34842c4a0b08eceb
yronhayashida <yronhayashida@hotmail.com>
2019-08-22 20:49:02 -0300

    Created second lib

---
+++ MyProject1/MyProject1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Second;
+
+namespace MyProject1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertNewButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = Convert.ToString(NameTextBox.Text);
+            int cellPhone = Convert.ToInt32(CellPhoneTextBox.Text);
+            string resultName;
+            string resultCellphone;
+
+            SecondFile validations = new SecondFile();
+            resultName = validations.validarNome(name);
+            resultCellphone = validations.validarCelular(cellPhone);
+            nameTextBlock.Text = string.Format("Nome: {0}", resultName);
+            CellTextBox.Text = string.Format("Celular: {0}", resultCellphone);
+        }
+    }
+}
---
+++ Second/Second/SecondFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    public class SecondFile
+    {
+
+        public string validarNome(string nome)
+        {
+            string name = nome;
+
+            if (nome == string.Empty)
+            {
+                return "campo nome não pode estar vazio";
+            }
+            return name;
+        }
+
+        public string validarCelular(int? cell)
+        {
+            if (cell == null)
+            {
+                return "campo Celular não pode estar vazio";
+            }
+            return Convert.ToString(cell);
+        }
+    }
+}
